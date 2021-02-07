4c57ef1139cd733ea7fdfc53b906002e4d1f8065
1aliff <xy.aliff@gmail.com>
2021-02-07 15:29:43 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+// import customerRoutes from './routes/apiRoutes.js'
+
+const app = express()
+const port = 5000
+
+app.use(bodyParser.json())
+
+// app.use('/customers', customerRoutes);
+
+app.get('/', (req, res) => { 
+  res.send('this is a response from homepage')
+})
+
+app.listen(port, () => console.log(`listening to ${port}`))
