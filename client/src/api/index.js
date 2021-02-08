674310760c31b97477291d94adf26241080d8b86
1aliff <xy.aliff@gmail.com>
2021-02-08 09:52:20 +0800

    Update Customer function

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -29,7 +29,6 @@ export const createCustomer = async (firstName, lastName, age, location) => {
   }
 }
 
-
 export const deleteUser = async (id) => {
   try {
     const result = await fetch(`/customers/${id}`, {
@@ -42,6 +41,25 @@ export const deleteUser = async (id) => {
       })
     })
     return await result
+  } catch (err) {
+    alert(`Something went wrong: ${err}`)
+  }
+}
+
+export const updateCustomer = async (id, firstName, lastName, age, location) => {
+  try {
+    const result = await fetch(`/customers/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        age,
+        location
+      })
+    })
   } catch (err) {
     alert(`Something went wrong: ${err}`)
   }
