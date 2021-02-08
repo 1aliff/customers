97308fb0bee67a9fff8508ab2d1d359d71888550
1aliff <xy.aliff@gmail.com>
2021-02-08 14:52:03 +0800

    Bug Fix

--- client/src/components/TableData/TableData.js
+++ client/src/components/TableData/TableData.js
@@ -36,13 +36,16 @@ const TableData = ({ customers }) => {
   const classes = useStyles();
   const total = customers.length
   const [open, setOpen] = useState(false) // modal state
+  const [customerUpdate, setCustomerUpdate] = useState([])
 
-  const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
-  const handleDelete = (id) => {
-    deleteUser(id)
+  const handleEdit = (customer) => {
+    setOpen(true)
+    setCustomerUpdate(customer)
   }
+
+  const handleDelete = (id) => deleteUser(id)
 
   return (
     <>
@@ -59,65 +62,59 @@ const TableData = ({ customers }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map((customer) => (
-              <TableRow>
-                <TableCell component="th" align="center" scope="row">{customer.id}</TableCell>
-                <TableCell align="center" key={customer.firstName} >{customer.firstName}</TableCell>
-                <TableCell align="center" key={customer.lastName} >{customer.lastName}</TableCell>
-                <TableCell align="center" key={customer.age} >{customer.age}</TableCell>
-                <TableCell align="center" key={customer.location}>{customer.location}</TableCell>
-                <TableCell align="center" key={customer.id + 1}>
-                <Button 
-                    variant="contained" 
-                    color="primary"
-                    style={{ marginRight: 10 }}
-                    onClick={() => handleOpen()}
-                  >
-                      Edit
+            {
+              customers.map((customer) => (
+                <TableRow>
+                  <TableCell component="th" align="center" scope="row">{customer.id}</TableCell>
+                  <TableCell align="center" key={customer.firstName} >{customer.firstName}</TableCell>
+                  <TableCell align="center" key={customer.lastName} >{customer.lastName}</TableCell>
+                  <TableCell align="center" key={customer.age} >{customer.age}</TableCell>
+                  <TableCell align="center" key={customer.location}>{customer.location}</TableCell>
+                  <TableCell align="center" key={customer.id + 1}>
+                  <Button variant="contained" color="primary" style={{ marginRight: 10 }} onClick={() => handleEdit(customer)}>
+                    Edit
                   </Button>
 
-                  {/* modal */}
-                    <Modal
-                      aria-labelledby="transition-modal-title"
-                      aria-describedby="transition-modal-description"
-                      className={classes.modal}
-                      open={open}
-                      onClose={handleClose}
-                      closeAfterTransition
-                      BackdropComponent={Backdrop}
-                      BackdropProps={{
-                        timeout: 500,
-                      }}
-                    >
-                      <Fade in={open}>
-                        <div className={classes.paper}>
-                          <UpdateForm
-                            id="transition-modal-title"
-                            handleClose={handleClose}
-                            customer={customer}
-                          />
-                        </div>
-                      </Fade>
-                    </Modal>
-
-                  <Button 
-                    variant="contained" 
-                    color="secondary" 
-                    onClick={() => 
-                      window.confirm(`Are you sure you want to delete this?`) && 
-                      handleDelete(customer.id)}
-                  >
-                      Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
+                    <Button variant="contained" color="secondary" 
+                      onClick={() => 
+                        window.confirm(`Are you sure you want to delete this?`) && 
+                        handleDelete(customer.id)}
+                    > Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            }
           </TableBody>
         </Table>
       </TableContainer>
       <div align="right" style={{ marginTop: 25, marginRight: 25 }}>
         <Typography variant="caption" gutterBottom>{`Showing ${total} results`}</Typography>
       </div>
+      
+      {/* modal render */}
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <UpdateForm
+                id="transition-modal-title"
+                handleClose={handleClose}
+                customer={customerUpdate}
+              />
+            </div>
+          </Fade>
+        </Modal>
     </>
   )
 }
--- client/src/components/TableData/UpdateForm/UpdateForm.js
+++ client/src/components/TableData/UpdateForm/UpdateForm.js
@@ -14,16 +14,18 @@ const useStyles = makeStyles((theme) => ({
 
 const UpdateForm = ({ customer, handleClose }) => {
   const classes = useStyles();
-  const id = customer.id
-  const [firstName, setFirstName] = useState(customer.firstName)
-  const [lastName, setLastName] = useState(customer.lastName)
-  const [age, setAge] = useState(customer.age)
-  const [location, setLocation] = useState(customer.location)
+
+  const { id, firstName, lastName, age, location } = customer
+
+  const [updateFName, setUpdateFName] = useState(firstName)
+  const [updateLName, setUpdateLName] = useState(lastName)
+  const [updateAge, setUpdateAge] = useState(age)
+  const [updateLocation, setUpdateLocation] = useState(location)
   
   const handleUpdate = e => {
     e.preventDefault()
     
-    updateCustomer(id, firstName, lastName, age, location).then(res => console.log(res))
+    updateCustomer(id, updateFName, updateLName, updateAge, updateLocation).then(res => console.log(res))
 
     handleClose(true)
   }
@@ -32,10 +34,10 @@ const UpdateForm = ({ customer, handleClose }) => {
     <>
       <form className={classes.root} noValidate autoComplete="off" onSubmit={handleUpdate}>
         <Typography variant="h5">Edit Customer Details</Typography>
-        <TextField id="filled-basic" label={customer.firstName} variant="filled" onChange={e => setFirstName(e.target.value)}/>
-        <TextField id="filled-basic" label={customer.lastName} variant="filled" onChange={e => setLastName(e.target.value)}/>
-        <TextField id="outlined-basic" label={customer.age} type="number" variant="outlined" onChange={e => setAge(e.target.value)}/>
-        <TextField id="filled-basic" label={customer.location} variant="filled" onChange={e => setLocation(e.target.value)}/>
+        <TextField id="filled-basic" label={updateFName} variant="filled" onChange={e => setUpdateFName(e.target.value)}/>
+        <TextField id="filled-basic" label={updateLName} variant="filled" onChange={e => setUpdateLName(e.target.value)}/>
+        <TextField id="outlined-basic" label={updateAge} type="number" variant="outlined" onChange={e => setUpdateAge(e.target.value)}/>
+        <TextField id="filled-basic" label={updateLocation} variant="filled" onChange={e => setUpdateLocation(e.target.value)}/>
         <Button variant="contained" color="primary" type="submit">Update</Button>
       </form>      
     </>
